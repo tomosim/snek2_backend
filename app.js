c81fefa8b728aa2cc637af47f5c2ea269285cc9d
Tom <TomSimmons@Toms-MacBook-Pro.local>
2018-11-15 17:17:09 +0000

    add cors

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const apiRouter = require("./routes");
 const mongoose = require("mongoose");
 const DB_URL = process.env.DB_URL || require("./config").DB_URL;
 const bodyParser = require("body-parser");
+const cors = require("cors");
+
 mongoose.connect(
   DB_URL,
   { useNewUrlParser: true }
 );
+app.use(cors());
 app.use(bodyParser.json());
 app.use("/api/", apiRouter);
 app.get("/*", (req, res, next) => res.status(400).send({ msg: "not found" }));
